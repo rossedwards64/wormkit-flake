5b9b56fda3e52ea6aaf861d143f283ab85274bc4
Ross Edwards <redwards64@hotmail.com>
2024-09-13 12:33:27 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,163 @@
+{
+  description = "Development environment for creating WormKit modules on Linux, in Rust.";
+
+  inputs = {
+    nixpkgs.url = "github:nixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    msvc-wine-rust = {
+      url = "github:est31/msvc-wine-rust";
+      flake = false;
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      msvc-wine-rust,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        toolchain = "stable";
+        host = "x86_64-unknown-linux-gnu";
+        target = "i686-pc-windows-msvc";
+
+        pkgsCrossMingw = pkgs.pkgsCross.mingw32;
+        mingw_w64 = pkgsCrossMingw.windows.mingw_w64;
+        mingw_w64_pthreads = pkgsCrossMingw.windows.pthreads.overrideAttrs (oldAttrs: {
+          configureFlags = (oldAttrs.configureFlags or [ ]) ++ [ "--enable-static" ];
+        });
+
+        msvcBaseDir = "/tmp/msvc";
+        msvcDownloadDir = "${msvcBaseDir}/dl";
+        msvcExtractedDir = "${msvcBaseDir}/extracted";
+        acceptedLicensesFile = "${pkgs.writeText "licenses-accepted" ""}";
+
+        msvcDownloadSha256 = pkgs.writeText "dl.sha256" (
+          builtins.replaceStrings [ "dl" ] [ "${msvcDownloadDir}" ] (
+            builtins.readFile "${msvc-wine-rust}/dl.sha256"
+          )
+        );
+
+        dlScript = pkgs.writeShellApplication {
+          name = "get";
+          runtimeInputs = [ pkgs.wget ];
+          excludeShellChecks = [ "SC2068" ];
+          extraShellCheckFlags = [ "--severity=error" ];
+          text = (
+            builtins.replaceStrings
+              [
+                "#!/usr/bin/env bash"
+                "-P dl"
+                "mkdir -p dl"
+                "mkdir -p extracted"
+                "-so dl/"
+                "> extracted"
+                "-f licenses-accepted"
+                "touch licenses-accepted"
+                "extracted/"
+                "dl.sha256"
+                "dl/microsoft"
+                "dl/Windows"
+                "dl/Universal"
+              ]
+              [
+                ""
+                "-P ${msvcDownloadDir}"
+                "mkdir -p ${msvcDownloadDir}"
+                "mkdir -p ${msvcExtractedDir}"
+                "-so ${msvcDownloadDir}/"
+                "> ${msvcExtractedDir}"
+                "-f ${acceptedLicensesFile}"
+                ":" # noop
+                "${msvcExtractedDir}/"
+                "${msvcDownloadSha256}"
+                "${msvcDownloadDir}/microsoft"
+                "${msvcDownloadDir}/Windows"
+                "${msvcDownloadDir}/Universal"
+              ]
+              (builtins.readFile "${msvc-wine-rust}/get.sh")
+          );
+        };
+
+        linkerScript = pkgs.writeShellApplication {
+          name = "linker";
+          runtimeInputs = [ pkgs.wine ];
+          excludeShellChecks = [ "SC2068" ];
+          extraShellCheckFlags = [ "--severity=error" ];
+          text = (
+            builtins.replaceStrings
+              [
+                "#!/usr/bin/env bash"
+                "$( cd \"$( dirname \"\${BASH_SOURCE[0]}\" )\" && cd ../extracted && pwd )"
+                "=x64"
+              ]
+              [
+                ""
+                "${msvcExtractedDir}"
+                "=x86"
+              ]
+              (builtins.readFile "${msvc-wine-rust}/linker-scripts/linker.sh")
+          );
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cargo
+            clippy
+            msitools
+            pkgsCrossMingw.stdenv.cc
+            rust-analyzer
+            rustfmt
+            rustup
+            wine
+          ];
+
+          WINEARCH = "win32";
+          RUSTUP_TOOLCHAIN = toolchain;
+          CARGO_BUILD_TARGET = target;
+          CARGO_TARGET_I686_PC_WINDOWS_MSVC_RUNNER = "${pkgs.wine}/bin/wine";
+          CARGO_TARGET_I686_PC_WINDOWS_MSVC_LINKER = "${linkerScript}/bin/linker";
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+          RUSTFLAGS =
+            (builtins.map (path: "-L ${path}/lib") [
+              mingw_w64
+              mingw_w64_pthreads
+            ])
+            ++ [ "-Clink-arg=/DEBUG:NONE" ];
+
+          shellHook = ''
+            set -o nounset
+            set -o pipefail
+
+            proj_dir=$(pwd)
+            if [ ! -d ${msvcBaseDir} ]
+            then
+                pushd "${msvc-wine-rust}"
+                ${dlScript}/bin/get licenses-accepted
+                popd
+            fi
+
+            # taken from https://github.com/lucasew/nixcfg/blob/master/nix/pkgs/wrapWine.nix
+            HOME="$(echo ~)"
+            export WINEPREFIX="$HOME/.local/share/wineprefixes/default32"
+            mkdir -p "$WINEPREFIX"
+            if [ ! -d "$WINEPREFIX" ]
+            then
+                wine cmd /c dir > /dev/null 2> /dev/null
+                wineserver -w
+            fi
+            export PATH=$PATH:$RUSTUP_HOME/toolchains/${toolchain}-${host}/bin/
+            rustup target add "${target}"
+          '';
+        };
+      }
+    );
+}
---
+++ src/lib.rs
@@ -0,0 +1,101 @@
+#![allow(non_snake_case)]
+
+use std::ffi::c_void;
+
+use windows::{
+    core::{self, PCWSTR},
+    Win32::{
+        Foundation::{self, HINSTANCE},
+        Storage::FileSystem::{self},
+        System::LibraryLoader,
+        System::SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH},
+        UI,
+    },
+};
+
+const VV1: u32 = 3;
+const VV2: u32 = 8;
+const VV3: u32 = 1;
+
+const DW_SIGNATURE: u32 = 0xFEEF04BD;
+
+macro_rules! hiword {
+    ($l:expr) => {
+        $l & 0xffff
+    };
+}
+
+macro_rules! loword {
+    ($l:expr) => {
+        ($l >> 16) & 0xffff
+    };
+}
+
+#[no_mangle]
+#[allow(unused_variables, improper_ctypes_definitions)]
+pub extern "system" fn DllMain(dll_module: HINSTANCE, call_reason: u32, _: *mut c_void) -> bool {
+    match call_reason {
+        DLL_PROCESS_ATTACH => {
+            unsafe {
+                UI::WindowsAndMessaging::MessageBoxW(
+                    Foundation::HWND::default(),
+                    core::w!("The version of your game is 3.8.1."),
+                    core::w!("wkVersionCheck"),
+                    UI::WindowsAndMessaging::MB_OK | UI::WindowsAndMessaging::MB_ICONERROR,
+                );
+            }
+            true
+        }
+        DLL_PROCESS_DETACH => detach(),
+        _ => false,
+    }
+}
+
+fn check_version(dll_module: HINSTANCE) -> bool {
+    unsafe {
+        let mut mod_info = make_buffer();
+        let mod_info_buf = mod_info.as_mut_slice();
+        LibraryLoader::GetModuleFileNameW(dll_module, mod_info_buf);
+
+        let mod_name = PCWSTR::from_raw(mod_info_buf.as_ptr());
+        let handle: Option<*mut u32> = Some(&mut u32::default());
+        let size = FileSystem::GetFileVersionInfoSizeW(mod_name, handle);
+
+        if size != 0 {
+            let ver_info_buf = make_buffer().as_mut_ptr();
+
+            let _ = FileSystem::GetFileVersionInfoW(mod_name, u32::default(), size, ver_info_buf);
+
+            let mut file_info =
+                &mut FileSystem::VS_FIXEDFILEINFO::default() as *mut _ as *mut c_void;
+
+            if FileSystem::VerQueryValueW(
+                ver_info_buf,
+                core::w!(r"\"),
+                &mut file_info,
+                &mut u32::default(),
+            )
+            .as_bool()
+            {
+                let file_info = *(file_info as *mut FileSystem::VS_FIXEDFILEINFO);
+
+                file_info.dwSignature == DW_SIGNATURE
+                    && hiword!(file_info.dwFileVersionMS) == VV1
+                    && loword!(file_info.dwFileVersionMS) == VV2
+                    && hiword!(file_info.dwFileVersionLS) == VV3
+            } else {
+                panic!("Couldn't get version info from file!")
+            }
+        } else {
+            panic!("Error getting file info size!")
+        }
+    }
+}
+
+fn detach() -> bool {
+    true
+}
+
+fn make_buffer<T>() -> Vec<T> {
+    Vec::<T>::with_capacity(Foundation::MAX_PATH as usize)
+}
